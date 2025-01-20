6f237d405d4aecfbf20250773a05e4342211072f
Paloma Nunes <45646844+palomsiq@users.noreply.github.com>
2025-01-20 10:19:48 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
+  function openWhats(){
+    window.open('https://wa.me/5581987469623'); 
+  }
+popupWhatsApp = () => {
+  
+    let btnClosePopup = document.querySelector('.closePopup');
+    let btnOpenPopup = document.querySelector('.whatsapp-button');
+    let popup = document.querySelector('.popup-whatsapp');
+    let sendBtn = document.getElementById('send-btn');
+  
+    btnClosePopup.addEventListener("click",  () => {
+      popup.classList.toggle('is-active-whatsapp-popup')
+    })
+    
+    btnOpenPopup.addEventListener("click",  () => {
+      popup.classList.toggle('is-active-whatsapp-popup')
+       popup.style.animation = "fadeIn .6s 0.0s both";
+    })
+    
+    sendBtn.addEventListener("click", () => {
+    let msg = document.getElementById('whats-in').value;
+    let relmsg = msg.replace(/ /g,"%20");
+      //just change the numbers "55819875469623" for your number. Don't use +001-(555)1234567     
+     window.open('https://wa.me/5581987469623?text='+relmsg, '_blank'); 
+    
+    });
+  
+    setTimeout(() => {
+      popup.classList.toggle('is-active-whatsapp-popup');
+    }, 3000);
+  }
+  
